099a4cf50e55f5fc5ac9ca6b3f1139f254c35bd0
andreTYS <andretys@gmail.com>
2026-04-16 19:52:57 -0500

    chore: add real Firebase options from flutterfire configure

--- app/lib/firebase_options.dart
+++ app/lib/firebase_options.dart
@@ -28,22 +28,24 @@ class DefaultFirebaseOptions {
     }
   }
 
-  // REEMPLAZA ESTOS VALORES CON LOS DE TU PROYECTO FIREBASE
   static const FirebaseOptions web = FirebaseOptions(
-    apiKey: 'TU_API_KEY_WEB',
-    appId: 'TU_APP_ID_WEB',
-    messagingSenderId: 'TU_SENDER_ID',
-    projectId: 'TU_PROJECT_ID',
-    authDomain: 'TU_PROJECT_ID.firebaseapp.com',
-    storageBucket: 'TU_PROJECT_ID.appspot.com',
+    apiKey: 'AIzaSyCo9YRMQtJxJxzzfDzaBXZam44qWIJIp80',
+    appId: '1:1036373712732:web:6382ee879c67c71674eb99',
+    messagingSenderId: '1036373712732',
+    projectId: 'contrataciones-790a0',
+    authDomain: 'contrataciones-790a0.firebaseapp.com',
+    storageBucket: 'contrataciones-790a0.firebasestorage.app',
+    measurementId: 'G-WFJE12HN6K',
   );
 
+  // REEMPLAZA ESTOS VALORES CON LOS DE TU PROYECTO FIREBASE
+
   static const FirebaseOptions android = FirebaseOptions(
-    apiKey: 'TU_API_KEY_ANDROID',
-    appId: 'TU_APP_ID_ANDROID',
-    messagingSenderId: 'TU_SENDER_ID',
-    projectId: 'TU_PROJECT_ID',
-    storageBucket: 'TU_PROJECT_ID.appspot.com',
+    apiKey: 'AIzaSyAHt06UwWVL2XLcJ3jCycIEvjIQqaL0kSw',
+    appId: '1:1036373712732:android:4075892a7732ba9474eb99',
+    messagingSenderId: '1036373712732',
+    projectId: 'contrataciones-790a0',
+    storageBucket: 'contrataciones-790a0.firebasestorage.app',
   );
 
   static const FirebaseOptions ios = FirebaseOptions(
